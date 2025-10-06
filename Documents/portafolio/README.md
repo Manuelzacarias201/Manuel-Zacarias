c978bad686fd975b0b39e9a69ce9dea009f902d7
manuelzacarias <zacariasmanuel501@gmail.com>
2025-10-06 17:15:55 -0600

    conectando de nuevo a mi terminal

---
+++ Documents/portafolio/README.md
@@ -0,0 +1,3 @@
+# Manuel-Zacarias
+Bienvenido a mi portafolio profesional como Programador Front-End: 
+https://manuelzacarias201.github.io/Manuel-Zacarias/
---
+++ Documents/portafolio/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('¡Gracias por tu mensaje! Me pondré en contacto contigo pronto.');
+});
